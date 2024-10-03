f9996397dbf3441fbf18882a092472f61ef06f7e
ava <kidstonek@gmail.com>
2024-10-03 06:59:10 +0300

    without menu

--- Ex_06_05_3.py
+++ Ex_06_05_3.py
@@ -1,3 +1,25 @@
+"""
+
+Завдання 2: Бібліотека книг та авторів
+Опис завдання:
+
+Створи класову структуру для керування бібліотекою. Має бути два основних класи: Author та Book.
+Клас Author представляє автора з його іменем і списком книг,
+а клас Book представляє окрему книгу з її назвою.
+
+Додатково, бібліотека повинна використовувати ітератори для зручної навігації та перегляду книг кожного автора.
+
+Задача:
+
+Створи клас Library, який міститиме списки книг та авторів.
+Реалізуй методи для додавання книг та авторів.
+Використовуй ітератор для перебору книг кожного автора.
+
+Також реалізуйте меню для виклуку кожної лдосутпної операц
+
+"""
+
+
 class Book:
     def __init__(self):
         self._whole_books = []
@@ -59,6 +81,9 @@ class Library:
         return self
 
     def __next__(self):
+        if len(self._content) == 0:
+            print("The library is empty")
+            raise StopIteration
         if self.current_index == len(self._content):
             raise StopIteration
 
@@ -89,7 +114,27 @@ if __name__ == "__main__":
     ath2.add_books(my_book2)
 
     lib = Library()
-    lib.add_items_to_library(ath1)
-    lib.add_items_to_library(ath2)
+    # lib.add_items_to_library(ath1)
+    # lib.add_items_to_library(ath2)
 
 
+def show_library(info: Library):   # for i in lib:
+    for item in info:
+        print(item)
+
+
+while True:
+    print("choose what to do:\n1) Show library:\n2) Add element to library\n3) Show all Books\n4) Show all Authors")
+    option = input('choose options: ')
+    if option == '1':
+        print()
+        show_library(lib)
+    elif option == '2':
+        pass
+    elif option == '3':
+        pass
+    elif option == '4':
+        pass
+    else:
+        print('end!')
+        break
